8aed5b5a78937332f853e10e9768cdffe4046dc4
daenamcclintock <daena.mcclintock@gmail.com>
2022-03-22 10:22:21 -0400

    Update for Deployment

--- controllers/comment.js
+++ controllers/comment.js
@@ -21,17 +21,11 @@ const router = express.Router()
 router.post('/:nftId', (req, res) => {
     const nftId = req.params.nftId
     console.log('first comment body', req.body)
-    
-    // we'll adjust req.body to include an author
-    // the author's id will be the logged in user's id
     req.body.author = req.session.userId
     console.log('updated comment body', req.body)
-    // we'll find the nft with the nftId
     Nft.findById(nftId)
         .then(nft => {
-            // then we'll send req.body to the comments array
             nft.comments.push(req.body)
-            // save the nft
             return nft.save()
         })
         .then(nft => {
@@ -46,22 +40,14 @@ router.post('/:nftId', (req, res) => {
 })
 
 // DELETE -> to destroy a comment
-// we'll use two params to make our life easier
-// first the id of the nft, since we need to find it
-// then the id of the comment, since we want to delete it
 router.delete('/delete/:nftId/:commId', (req, res) => {
-    // first we want to parse out our ids
     const nftId = req.params.nftId
     const commId = req.params.commId
-    // then we'll find the nft
     Nft.findById(nftId)
         .then(nft => {
             const theComment = nft.comments.id(commId)
-            // only delete the comment if the user who is logged in is the comment's author
             if ( theComment.author == req.session.userId) {
-                // then we'll delete the comment
                 theComment.remove()
-                // return the saved NFT
                 return nft.save()
             } else {
                 return
--- models/connection.js
+++ models/connection.js
@@ -4,23 +4,27 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+// Connect to the Database
+// LOCAL DATABASE CONNECTION => process.env.DATABASE_URL
+// REMOTE DATABASE CONNECTION => process.env.MONGODB_URI
+
 /////////////////////////////////
 // database connection
 /////////////////////////////////
 // here we are setting up inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL
+const MONGODB_URI = process.env.MONGODB_URI
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
 
 // establish connection
-mongoose.connect(DATABASE_URL, CONFIG)
+mongoose.connect(MONGODB_URI, CONFIG)
 
 // events for when our connection opens/closes/errors
 mongoose.connection
-	.on('open', () => console.log('Connected to Mongoose'))
-	.on('close', () => console.log('Disconnected from Mongoose'))
+	.on('open', () => console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`))
+	.on('close', () => console.log(`Disconnected from ${mongoose.connection.host}:${mongoose.connection.port}`))
 	.on('error', (error) => console.log(error))
 
 /////////////////////////////////
